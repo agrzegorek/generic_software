4a86fb8f7ed41d32f51c056f5007f4b082b8d4bf
Andrew Grzegorek <agrzegorek@vitalhuber.local>
2018-03-01 15:50:35 -0600

    Added navbar options to Solutions section. Changed tabs to hover based instead of click

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function( $ ) {
+
+	// =====Solutions NavBar Hover Function======
+	$(".nav-tabs li").hover(function() {
+		$(this).parent().children().removeClass("active");
+		$(this).toggleClass("active");
+
+		$(".tab-content div").removeClass("in show active");
+		var selectedTab = $(this).children('a').attr('href');
+		$("div.tab-pane" + selectedTab).addClass("in show active");
+	});
+
+});
