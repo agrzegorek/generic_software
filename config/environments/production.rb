b860776c05b04db23776a04f58f36f125bdfafea
Andrew Grzegorek <andrewgrz@gmail.com>
2018-04-06 15:09:43 -0500

    Update issue with rails master key for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  config.read_encrypted_secrets = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -86,6 +86,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # RAILS_MASTER_KEY="c4f166984415e051f339226216346faf"
+  RAILS_MASTER_KEY="c4f166984415e051f339226216346faf"
   # GOOGLE_API_KEY="AIzaSyA31j7aTS5v0jQaFugN_Rx_rUaZvh6qHXU"
 end
